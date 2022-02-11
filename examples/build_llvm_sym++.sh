3e5fff3ee30c913edb7ba5477fb8fb793018c74e
Frédéric Tuong <ftuong@sfu.ca>
2022-02-10 20:53:25 -0800

    generate without the symcc pass

--- examples/build_llvm_sym++.sh
+++ examples/build_llvm_sym++.sh
@@ -8,6 +8,15 @@ set -euxo pipefail
 for dir in source*cpp
 do
     pushd "$dir"
+    mkdir -p generated/llvm/clang++_isystem
+
+    pushd generated/llvm/clang++_isystem
+    clang++-10 \
+        -isystem /libcxx_symcc_install/include/c++/v1 \
+        -stdlib=libc++ \
+        -S -emit-llvm "$@" ../../../sample.cpp
+    popd
+    
     mkdir -p generated/llvm/sym++
 
     pushd generated/llvm/sym++
