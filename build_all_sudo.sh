a1178b24f556ad77278a0e22e7b2c7b85bc8e017
Frédéric Tuong <ftuong@sfu.ca>
2022-05-02 17:55:40 -0700

    prevent from executing on too low disk space

--- build_all_sudo.sh
+++ build_all_sudo.sh
@@ -5,6 +5,14 @@
 
 set -euo pipefail
 
+unit=G
+size=$(echo $(df -B"$unit" --output=avail $(docker info | grep 'Root' | cut -d ':' -f 2) | tail -n 1))
+
+if (( $(echo "$size" | cut -d "$unit" -f 1) < 30 )) ; then
+    echo "Error: too low remaining disk space: $size" >&2
+    exit 1
+fi
+
 function docker_b0 () {
     date -R
     /usr/bin/time -v docker build "$@"
