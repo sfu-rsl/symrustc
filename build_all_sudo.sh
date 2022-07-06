abd38718318a40dc83e7790b196e8b8a5f6925aa
Frédéric Tuong <ftuong@sfu.ca>
2022-07-05 20:37:21 -0700

    upgrade to 1.62.1

--- build_all_sudo.sh
+++ build_all_sudo.sh
@@ -24,7 +24,7 @@ export -f docker_b0
 #
 
 function docker_b () {
-    docker_b0 --target "builder_$1" -t "belcarra_$1" --build-arg SYMRUSTC_BRANCH="$SYMRUSTC_BRANCH" --build-arg SYMRUSTC_SKIP_FAIL=yes .
+    docker_b0 --target "builder_$1" -t "belcarra_$1" --build-arg SYMRUSTC_BRANCH="$SYMRUSTC_BRANCH" --build-arg SYMRUSTC_SKIP_FAIL=yes --build-arg SYMRUSTC_LIBAFL_EXAMPLE_SKIP_BUILD_TRACING=yes .
 }
 export -f docker_b
 
