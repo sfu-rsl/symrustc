b343d0e49fda16e65169691aebbf9a12d889c265
Frédéric Tuong <ftuong@sfu.ca>
2022-07-14 11:17:28 -0700

    add a verbose version

---
+++ examples/source_0_original_1b0_rs/src/main.rs
@@ -0,0 +1,35 @@
+/* The LLVM code obtained from this program would ideally be as close
+   as possible to the one obtained from:
+   https://github.com/eurecom-s3/symcc/blob/master/sample.cpp
+*/
+
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+
+fn main() -> Result<(), io::Error> {
+    let mut buf = vec![];
+    let delim = b"\n";
+    let root = b"root";
+    let root_len = root.len();
+    println!("What's your name?");
+    match BufReader::new(io::stdin()).read_until(delim[0], &mut buf) {
+        Ok(_) => {
+            let buf_len = buf.len();
+            if buf_len >= root_len
+                && buf[0 .. root_len] == *root
+                && if buf_len == root_len { true }
+                   else if buf_len == root_len + 1 { buf.ends_with(delim) }
+                   else { false } {
+                println!("What is your command?")
+            } else {
+                print!("Hello, (");
+                for c in &buf {
+                    print!(" {:#04x}", c)
+                }
+                println!(" ) {:?}!", String::from_utf8_lossy(&buf))
+            }
+            Ok(())
+        }
+        Err(error) => Err(error)
+    }
+}
