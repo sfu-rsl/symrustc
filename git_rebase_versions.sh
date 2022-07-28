7f77b75154499a37414955121dd2e9abea7e4f9e
Frédéric Tuong <ftuong@sfu.ca>
2022-07-27 17:29:54 -0700

    add rebase script

--- git_rebase_versions.sh
+++ git_rebase_versions.sh
@@ -37,6 +37,8 @@ function git_rebase_push2 () {
 declare -a git_count=()
 
 version0=1.46.0
+version1=1.55.0
+version2=rust_runtime/1.55.0
 versions=( 1.47.0 \
            1.48.0 \
            1.49.0 \
@@ -45,7 +47,7 @@ versions=( 1.47.0 \
            1.52.1 \
            1.53.0 \
            1.54.0 \
-           1.55.0 \
+           $version1 \
            1.56.1 \
            1.57.0 \
            1.58.1 \
@@ -63,12 +65,15 @@ do
     git_count+=("$(git rev-list --count ^"$remote/$br1" "$remote/$br2")")
     br1=${br2}
 done
+git_count+=("$(git rev-list --count ^"$remote/$version1" "$remote/$version2")")
 
 #
 
 git push --force-with-lease "$remote" $version0
 git_rebase_push2 $version0 full_runtime/1.46.0
 git_rebase_push2 $version0 verbose/1.46.0
+
+#
 
 br1=$version0
 for br2 in "${versions[@]}"
@@ -77,5 +82,9 @@ do
     git_count=("${git_count[@]:1}")
     br1=${br2}
 done
+git_rebase_push $version1 $version2 "${git_count[0]}"
+git_count=("${git_count[@]:1}")
+
+#
 
 git checkout $version0
