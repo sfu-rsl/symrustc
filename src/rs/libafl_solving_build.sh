a147c8de38493bf0036225c4d7f67b68fa5d9a9e
Frédéric Tuong <ftuong@sfu.ca>
2022-08-29 19:05:01 -0700

    generalize

--- src/rs/libafl_solving_build.sh
+++ src/rs/libafl_solving_build.sh
@@ -5,9 +5,16 @@
 
 set -euo pipefail
 
+#
+
+source $SYMRUSTC_HOME_RS/parse_args.sh
+
+#
+
 if [[ -v SYMRUSTC_LIBAFL_EXAMPLE_SKIP_BUILD_SOLVING ]] ; then
+    cd -P $SYMRUSTC_DIR
     mkdir -p target_cargo_on/debug
-    curl -LO 'https://github.com/sfu-rsl/symrustc/raw/1.46.0_binary/'"$(echo $SYMRUSTC_LIBAFL_EXAMPLE | rev | cut -d / -f 1-2 | rev)"'/belcarra'
+    curl -LO 'https://github.com/sfu-rsl/symrustc/raw/1.46.0_binary/'"$(echo $PWD | rev | cut -d / -f 1-2 | rev)"'/belcarra'
     chmod +x belcarra
     mv belcarra target_cargo_on/debug
 else
