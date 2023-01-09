a5c9f8dc47a176446bc14193b15eb7d2d8b564b4
Frédéric Tuong <ftuong@sfu.ca>
2023-01-09 13:32:41 -0800

    add new tracking branches

--- git_rebase_versions.sh
+++ git_rebase_versions.sh
@@ -48,34 +48,35 @@ function git_rebase_push () {
 declare -a git_count=()
 
 version0=1.46.0
-version1a=no_insert/1.55.0
+version1a=1.55.0
 version2a=rust_runtime/1.55.0
-version1b=no_insert/1.62.1
+version1b=1.62.1
 version2b=extended_examples/1.62.1
-version1c=no_insert/1.63.0
+version1c=1.63.0
 version2c=extended_examples/1.63.0
-versions=( ubuntu_20_04/1.47.0 \
-           1.47.0 \
-           no_insert/1.48.0 \
-           no_insert/1.49.0 \
-           no_insert/1.50.0 \
-           no_insert/1.51.0 \
-           no_insert/1.52.1 \
-           no_insert/1.53.0 \
-           no_insert/1.54.0 \
-           $version1a \
-           no_insert/1.56.1 \
-           no_insert/1.57.0 \
-           no_insert/1.58.1 \
-           no_insert/1.59.0 \
-           no_insert/1.60.0 \
-           no_insert/1.61.0 \
-           $version1b \
-           $version1c \
-           no_insert/1.64.0 \
-           no_insert/ubuntu_20_10/1.64.0 \
-           no_insert/1.65.0 \
-           no_insert/1.66.0 )
+
+versions0=( ubuntu_20_04/1.47.0 \
+            1.47.0 )
+versions1=( 1.48.0 \
+            1.49.0 \
+            1.50.0 \
+            1.51.0 \
+            1.52.1 \
+            1.53.0 \
+            1.54.0 \
+            $version1a \
+            1.56.1 \
+            1.57.0 \
+            1.58.1 \
+            1.59.0 \
+            1.60.0 \
+            1.61.0 \
+            $version1b \
+            $version1c \
+            1.64.0 \
+            ubuntu_20_10/1.64.0 \
+            1.65.0 \
+            1.66.0 )
 
 #
 
@@ -83,14 +84,28 @@ git_count+=("$(git rev-list --count ^"$remote/$version0" "$remote/full_runtime/1
 git_count+=("$(git rev-list --count ^"$remote/$version0" "$remote/verbose/1.46.0")")
 
 br1=$version0
-for br2 in "${versions[@]}"
+for br2 in "${versions0[@]}"
 do
     git_count+=("$(git rev-list --count ^"$remote/$br1" "$remote/$br2")")
     br1=${br2}
 done
-git_count+=("$(git rev-list --count ^"$remote/$version1a" "$remote/$version2a")")
-git_count+=("$(git rev-list --count ^"$remote/$version1b" "$remote/$version2b")")
-git_count+=("$(git rev-list --count ^"$remote/$version1c" "$remote/$version2c")")
+br0=${br1}
+for br2 in "${versions1[@]}"
+do
+    br2="no_insert/${br2}"
+    git_count+=("$(git rev-list --count ^"$remote/$br1" "$remote/$br2")")
+    br1=${br2}
+done
+git_count+=("$(git rev-list --count ^"$remote/no_insert/$version1a" "$remote/$version2a")")
+git_count+=("$(git rev-list --count ^"$remote/no_insert/$version1b" "$remote/$version2b")")
+git_count+=("$(git rev-list --count ^"$remote/no_insert/$version1c" "$remote/$version2c")")
+
+br1=${br0}
+for br2 in "${versions1[@]}"
+do
+    git_count+=("$(git rev-list --count ^"$remote/$br1" "$remote/$br2")")
+    br1=${br2}
+done
 
 #
 
@@ -102,14 +117,28 @@ git_rebase_push $version0 full_runtime/1.46.0 "${git_count[0]}"; git_count=("${g
 git_rebase_push $version0 verbose/1.46.0 "${git_count[0]}"; git_count=("${git_count[@]:1}")
 
 br1=$version0
-for br2 in "${versions[@]}"
+for br2 in "${versions0[@]}"
 do
     git_rebase_push $br1 $br2 "${git_count[0]}"; git_count=("${git_count[@]:1}")
     br1=${br2}
 done
-git_rebase_push $version1a $version2a "${git_count[0]}"; git_count=("${git_count[@]:1}")
-git_rebase_push $version1b $version2b "${git_count[0]}"; git_count=("${git_count[@]:1}")
-git_rebase_push $version1c $version2c "${git_count[0]}"; git_count=("${git_count[@]:1}")
+br0=${br1}
+for br2 in "${versions1[@]}"
+do
+    br2="no_insert/${br2}"
+    git_rebase_push $br1 $br2 "${git_count[0]}"; git_count=("${git_count[@]:1}")
+    br1=${br2}
+done
+git_rebase_push no_insert/$version1a $version2a "${git_count[0]}"; git_count=("${git_count[@]:1}")
+git_rebase_push no_insert/$version1b $version2b "${git_count[0]}"; git_count=("${git_count[@]:1}")
+git_rebase_push no_insert/$version1c $version2c "${git_count[0]}"; git_count=("${git_count[@]:1}")
+
+br1=${br0}
+for br2 in "${versions1[@]}"
+do
+    git_rebase_push $br1 $br2 "${git_count[0]}"; git_count=("${git_count[@]:1}")
+    br1=${br2}
+done
 
 #
 
