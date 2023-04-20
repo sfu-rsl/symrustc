4d34a6af23f09bb4d34ddaca84e9963dafb0f6c6
Frédéric Tuong <ftuong@sfu.ca>
2023-04-19 21:53:22 -0700

    build the concolic example as an inline Rust source

--- examples/source_0_original_1c0_rs/src/main.rs
+++ examples/source_0_original_1c0_rs/src/main.rs
@@ -1,6 +1,17 @@
-use std;
+use std::io;
+use std::env;
+use std::fs;
 use bel;
 
-fn main() -> Result<(), std::io::Error> {
-    bel::main0()
+fn main() -> Result<(), io::Error> {
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        Some(file) => {
+            match bel::main0(fs::read(file).expect("Error reading the file")) {
+                Some(_) => Ok(()),
+                None => panic!("Artificial bug")
+            }
+        }
+        None => Ok(())
+    }
 }
