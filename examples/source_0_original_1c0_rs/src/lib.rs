6ff9fe8c5fc1516dd691ae9a2572dd6842f9dced
Frédéric Tuong <ftuong@sfu.ca>
2023-04-26 19:02:59 -0700

    generalize

--- examples/source_0_original_1c0_rs/src/lib.rs
+++ examples/source_0_original_1c0_rs/src/lib.rs
@@ -3,9 +3,16 @@
    https://github.com/eurecom-s3/symcc/blob/master/sample.cpp
 */
 
-use std::io;
+use std::ffi::OsString;
+use std::fs;
+use std::io::Error;
 
-pub fn main0(buf : Vec<u8>) -> Option<()> {
+fn main00(mut args : impl Iterator<Item = OsString>) -> Result<(), Error> {
+    match args.next() {
+        Some(_) => {
+    match args.next() {
+        Some(file) => {
+            let buf = fs::read(file).expect("Error reading the file");
             let buf_len = buf.len();
             let delim = b"\n";
             println!("What's your name?");
@@ -82,5 +89,15 @@ pub fn main0(buf : Vec<u8>) -> Option<()> {
                 }
                 println!(" ) {:?}!", String::from_utf8_lossy(&buf))
             }
-    Some(())
+            Ok(())
+        }
+        None => Ok(())
+    }
+    }
+        None => Ok(())
+    }
+}
+
+pub fn main0(args : Vec<OsString>) -> Result<(), Error> {
+    main00(args.iter().cloned())
 }
