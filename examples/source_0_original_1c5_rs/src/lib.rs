5ec885f303a8b3dc3e4a7ca6c6ca850249297d58
Frédéric Tuong <ftuong@sfu.ca>
2023-05-15 11:19:19 -0700

    add an example

---
+++ examples/source_0_original_1c5_rs/src/lib.rs
@@ -0,0 +1,24 @@
+use std::ffi::OsString;
+use std::fs;
+use std::io::Error;
+
+fn main00(mut args : impl Iterator<Item = OsString>) -> Result<(), Error> {
+    match args.next() {
+        Some(_) => {
+    match args.next() {
+        Some(file) => {
+            let buf = fs::read(file).expect("Error reading the file");
+            let s = String::from_utf8_lossy(&buf);
+            human_name::Name::parse(&s);
+            Ok(())
+        }
+        None => Ok(())
+    }
+    }
+        None => Ok(())
+    }
+}
+
+pub fn main0(args : Vec<OsString>) -> Result<(), Error> {
+    main00(args.iter().cloned())
+}
