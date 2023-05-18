51e28b053dff4344a90b464f3b97c609745ae581
Frédéric Tuong <ftuong@sfu.ca>
2023-05-18 16:15:12 -0700

    update the generated files

--- generated/build.sh
+++ generated/build.sh
@@ -32,20 +32,8 @@ docker_b symrustc
 # Build SymRustC main
 docker_b symrustc_main
 
-# Build concolic Rust examples
-docker_b examples_rs
-
 # Set up Ubuntu/Rust environment
 docker_b base_rust
-
-# Build LibAFL tracing runtime
-docker_b libafl_tracing
-
-# Build LibAFL tracing runtime main
-docker_b libafl_tracing_main
-
-# Build concolic Rust examples for LibAFL tracing
-docker_b libafl_tracing_example
 
 # Build LibAFL solving runtime
 docker_b libafl_solving
@@ -55,55 +43,4 @@ docker_b libafl_solving_main
 
 # Build concolic Rust examples for LibAFL solving
 docker_b libafl_solving_example
-
-# Build additional tools
-docker_b addons
-
-# Build cargo-fuzz
-docker_b cargo_fuzz
-
-# Build concolic Rust examples - set up project source - local example - fuzz
-docker_b examples_rs_source_local_fuzz
-
-# Build concolic Rust examples - set up project source - coreutils
-docker_b examples_rs_source_coreutils
-
-# Build concolic Rust examples - set up project source - coreutils - fuzz
-docker_b examples_rs_source_coreutils_fuzz
-
-# Build concolic Rust examples - set up project source - bincode
-docker_b examples_rs_source_bincode
-
-# Build concolic Rust examples - set up project source - bincode - fuzz
-docker_b examples_rs_source_bincode_fuzz
-
-# Build concolic Rust examples - set up project source - coreutils - libafl
-docker_b examples_rs_source_coreutils_libafl
-
-# Build concolic Rust examples - coreutils
-docker_b examples_rs_coreutils
-
-# Build concolic Rust examples - coreutils - libafl
-docker_b examples_rs_coreutils_libafl
-
-# Build concolic Rust examples - set up project source - linux
-docker_b examples_rs_source_linux
-
-# Build concolic Rust examples - linux
-docker_b examples_rs_linux
-
-# Build extended main
-docker_b extended_main
-
-# Build concolic C++ examples - SymCC/Z3, libcxx regular
-docker_b examples_cpp_z3_libcxx_reg
-
-# Build concolic C++ examples - SymCC/Z3, libcxx instrumented
-docker_b examples_cpp_z3_libcxx_inst
-
-# Build concolic C++ examples - SymCC/QSYM
-docker_b examples_cpp_qsym
-
-# Build concolic C++ examples - Only clang
-docker_b examples_cpp_clang
 
