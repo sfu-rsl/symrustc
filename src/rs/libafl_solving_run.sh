315ec76ecebfeeb61b63d73c5e415b1c0a290624
Frédéric Tuong <ftuong@sfu.ca>
2023-06-08 11:40:57 -0700

    identify the running example

--- src/rs/libafl_solving_run.sh
+++ src/rs/libafl_solving_run.sh
@@ -18,12 +18,17 @@ swap
 
 #
 
-fic_server=log_server
+date_now="$(date '+%F_%T' | tr -d ':-')"
+
+log_server_grep=log_server
+log_server="$HOME/libafl_${date_now}_server"
+log_client1="$HOME/libafl_${date_now}_client1"
+log_client2="$HOME/libafl_${date_now}_client2"
 
 pushd $SYMRUSTC_LIBAFL_SOLVING_DIR/fuzzer >/dev/null
 
 mkdir -p corpus
-fic_corpus="corpus/$(date '+%F_%T' | tr -d ':-')"
+fic_corpus="corpus/${date_now}"
 echo "$@" > $fic_corpus
 
 ln -s $(find -L $SYMRUSTC_DIR/target_cargo_on/debug -maxdepth 1 -type f -executable | grep . -m 1) target_symcc.out
@@ -49,12 +54,14 @@ EOF
 
 chmod +x $fuzz_bin_fic
 
+readlink -e harness > $log_server
+
 # starting the server
 if [[ -v SYMRUSTC_LIBAFL_SOLVING_OBJECTIVE ]] ; then
-    mkfifo $fic_server
-    $fuzz_bin_fic | tee ~/libafl_server | tee $fic_server &
+    mkfifo $log_server_grep
+    $fuzz_bin_fic | tee -a $log_server | tee $log_server_grep &
 else
-    $fuzz_bin_fic | tee ~/libafl_server &
+    $fuzz_bin_fic | tee -a $log_server &
 fi
 
 # waiting for the server to listen to new clients
@@ -63,15 +70,15 @@ while ! nc -zv localhost 1337 ; do
 done
 
 # starting the clients
-$fuzz_bin_fic --concolic >~/libafl_client1 2>&1 &
+$fuzz_bin_fic --concolic >$log_client1 2>&1 &
 proc_client1=$!
-$fuzz_bin_fic >~/libafl_client2 2>&1 &
+$fuzz_bin_fic >$log_client2 2>&1 &
 proc_client2=$!
 
 # waiting
 if [[ -v SYMRUSTC_LIBAFL_SOLVING_OBJECTIVE ]] ; then
     # waiting for a specific message from the server before proceeding further
-    grep -q 'objectives: 2' $fic_server
+    grep -q 'objectives: 2' $log_server_grep
 else
     echo "Only executing during a finite period of time, irrespective of objective search" >&2
     sleep 300
@@ -87,7 +94,7 @@ wait_all
 
 # cleaning
 if [[ -v SYMRUSTC_LIBAFL_SOLVING_OBJECTIVE ]] ; then
-    rm $fic_server
+    rm $log_server_grep
 fi
 rm $fic_corpus
 rm $fuzz_bin_fic
